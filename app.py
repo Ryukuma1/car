b3869caa69babd605ed5f503c2c8cc762fca9ff1
Ryukuma1 <145217308+Ryukuma1@users.noreply.github.com>
2023-09-27 03:52:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,22 @@ import plotly.express as px
 df = pd.read_csv('vehicles_us.csv')
 
 # Streamlit app header
-st.header("Car Price Distribution by type")
+st.header("Visualization App")
 
-# Allow users to choose car models to display
-selected_models = st.multiselect("Select Car Models to Display", df['type'].unique())
+# Allow users to choose car types or models to display in the histogram
+selected_types = st.multiselect("Select Car Types or Models", df['type'].unique())
 
-# Filter the dataset based on selected car models
-filtered_df = df[df['type'].isin(selected_models)]
+# Filter the dataset based on selected car types or models
+filtered_df = df[df['type'].isin(selected_types)]
 
-# Display the histogram with color-coded models
-st.write("Car Price Histogram:")
-fig = px.histogram(filtered_df, x='price', color='type', title='Car Price Distribution by model')
+# Check if any selections were made
+if len(selected_types) > 0:
+    st.write("Histogram:")
+    fig = px.histogram(filtered_df, x='price')
+    st.plotly_chart(fig)
+else:
+    st.write("Please select one or more car types or models to display the histogram.")
+
+st.write("Scatter Plot:")
+fig = px.scatter(df, x='price', y='type')
 st.plotly_chart(fig)
